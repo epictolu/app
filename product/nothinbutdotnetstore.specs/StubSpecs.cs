3b200c9ec33fe4192a6dbfb59cf4af6f17fdca9a
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-02-23 15:00:27 -0500

    Updated the Stub

---
+++ product/nothinbutdotnetstore.specs/StubSpecs.cs
@@ -0,0 +1,30 @@
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.infrastructure;
+
+namespace nothinbutdotnetstore.specs
+{
+    public class StubSpecs
+    {
+        public abstract class concern : Observes
+        {
+        }
+
+        [Subject(typeof(Stub))]
+        public class when_stubbing_a_provided_type : concern
+        {
+            Because b = () =>
+                result = Stub.with<MyItem>();
+
+            It should_return_an_instance_of_the_type_specified = () =>
+                result.ShouldBeAn<MyItem>();
+
+
+            static MyItem result;
+        }
+
+        public class MyItem
+        {
+        }
+    }
+}
--- product/nothinbutdotnetstore/infrastructure/Stub.cs
+++ product/nothinbutdotnetstore/infrastructure/Stub.cs
@@ -1,12 +1,10 @@
-using System;
-
 namespace nothinbutdotnetstore.infrastructure
 {
     public class Stub
     {
-        public static Fake with<Fake>()
+        public static Fake with<Fake>() where Fake : new()
         {
-            throw new NotImplementedException();
+            return new Fake();
         }
     }
 }

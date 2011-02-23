3274e81bcc3bf17ebbff43fb1c289b693cd7c1e6
JammerX19 <jodyfarr@yahoo.com>
2011-02-23 09:33:30 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
@@ -4,9 +4,18 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultFrontController : FrontController
     {
+        private CommandRegistry command_registry;
+
+        public DefaultFrontController(CommandRegistry registry)
+        {
+            this.command_registry = registry;
+        }
+
         public void process(Request request)
         {
-            throw new NotImplementedException();
+            RequestCommand rc = this.command_registry.get_command_that_can_process(request);
+            rc.run(request);
+            //throw new NotImplementedException();
         }
     }
 }

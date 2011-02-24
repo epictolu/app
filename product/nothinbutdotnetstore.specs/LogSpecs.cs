fb07ca8873c1b628855ec3afa776284d88d108bb
noronha1978 <noronha1978@yahoo.com>
2011-02-24 14:18:02 -0500

    Pushing new changes

---
+++ product/nothinbutdotnetstore.specs/LogSpecs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.specs.utility;
+using nothinbutdotnetstore.web.core;
+using Rhino.Mocks;
+
+namespace nothinbutdotnetstore.specs
+{
+    public class LogSpecs
+    {
+        public class concern : Observes<LoggingRequestFactory>
+        {
+            
+        }
+
+        public class When_logging_is_requested:concern
+        {
+            private Establish c = () =>
+                                      {
+                                          default_factory = the_dependency<RequestFactory>();
+                                          log = the_dependency<Log>();
+                                          incoming_request = ObjectFactory.Web.create_http_context();
+                                          default_factory.Stub(x => x.create_request_from(incoming_request)).Return(
+                                              resulting_request);
+
+                                      };
+
+            private Because b = () =>
+                                    {
+                                        sut.create_request_from(incoming_request);
+                                    };
+
+            private It should_log_a_message = () =>
+                                                  {
+                                                      log.received(x => x.message(resulting_request));
+                                                  };
+
+            private static RequestFactory default_factory;
+            private static Request resulting_request;
+            private static HttpContext incoming_request;
+            private static Log log;
+
+        }
+
+
+    }
+}
---
+++ product/nothinbutdotnetstore/web/core/LoggingRequestFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetstore.web.core
+{
+    public class LoggingRequestFactory:RequestFactory
+    {
+        private readonly RequestFactory _factory;
+        private readonly Log _logger;
+
+        public LoggingRequestFactory(RequestFactory factory, Log logger)
+        {
+            _factory = factory;
+            _logger = logger;
+        }
+
+        public Request create_request_from(System.Web.HttpContext incoming_context)
+        {
+            Request request = _factory.create_request_from(incoming_context);
+            _logger.message(request);
+            return request;
+        }
+    }
+}

05deace00d834f3d8c4331cb528b1b6538d1d6ef
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-05-10 23:21:14 -0400

    ignored all the files that should be ignored

--- init.ps1
+++ init.ps1
@@ -1,4 +1,3 @@
-
 "Creating the remote link to developwithpassion code base"
 git remote rm jp
-git remote add jp http://github.com/developwithpassion/munaprs.git
+git remote add jp http://github.com/developwithpassion/ascriptsa.git

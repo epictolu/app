ade1f76d935ee0ce706a088d8eccdbed93e9aca1
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-05-10 23:03:07 -0400

    Updated the latest

--- push_latest_work.ps1
+++ push_latest_work.ps1
@@ -1,12 +1,19 @@
-$status = git status
-
-if ($status[0].endswith("master"))
+function verify_on_master()
 {
-  "You need to run this on a non master branch"
-  exit
+  $status = git status
+
+  if ($status[0].endswith("master"))
+  {
+    "You need to run this on a non master branch"
+    exit
+  }
 }
 
+verify_on_master()
 git add -A
 git commit -m "Pushing new changes"
+verify_on_master()
 git push origin
+
+
 

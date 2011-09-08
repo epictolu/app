a1816b8980a403037d9079bcd6ef45333c03fae3
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-09-08 15:58:36 -0600

    Updated the main project file

--- build/configuration/project.rb
+++ build/configuration/project.rb
@@ -1,13 +1,13 @@
 config = 
 {
-  :course_name => missing("course_name",__FILE__),
+  :course_name => missing('course_name',__FILE__),
   :project => "nothinbutdotnetstore",
   :target => "Debug",
   :source_dir => "source",
   :all_references => UniqueFiles.new(Dir.glob("packages/**/*.{dll,exe}")).all_files,
   :artifacts_dir => "artifacts",
   :config_dir => "source/config",
-  :app_dir => delayed{"source/#{configatron.project}"},
+  :app_dir => delayed{"source/#{configatron.project}.web.ui"},
   :log_file_name => delayed{"#{configatron.project}_log.txt"},
   :log_level => "DEBUG"
 }

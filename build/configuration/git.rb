a05816405ea4fe8ffe73da06d153123ea4864b67
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-09-17 21:25:30 -0600

    Updated the git file

--- build/configuration/git.rb
+++ build/configuration/git.rb
@@ -1,7 +1,8 @@
 configs ={
   :git => {
+    :user => missing("user",__FILE__),
     :remotes => potentially_change("remotes",__FILE__),
-    :repo => missing("repo",__FILE__)
+    :repo => 'store'
   }
 }
 configatron.configure_from_hash(configs)

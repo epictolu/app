94b6fa374ca2840f8cefc9fc8b2f70fef7385ecd
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-11-08 17:31:49 -0700

    Updated the name of the repository

--- build/support/002_configuration/git.rb
+++ build/support/002_configuration/git.rb
@@ -2,7 +2,7 @@ configs ={
   :git => {
     :user => missing("user",__FILE__),
     :remotes => potentially_change("remotes",__FILE__),
-    :repo => 'prep' 
+    :repo => 'app' 
   }
 }
 configatron.configure_from_hash(configs)

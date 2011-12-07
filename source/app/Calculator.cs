7c74b7397299b922b6c9d5ab8e9543cd431ae68a
wizengamot <vbwizard@gmail.com>
2011-12-07 10:55:42 -0700

    Updated

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,12 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int first, int second)
     {
+        if (first < 0 || second < 0) throw new ArgumentException();
       return first + second;
     }
   }

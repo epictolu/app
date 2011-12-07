61c6436ad5495b52f51bd1cea915d5ca4af56906
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-07 15:20:16 -0700

    Updated

---
+++ source/app.specs/ProcessingCommandSpecs.cs
@@ -0,0 +1,36 @@
+﻿ using Machine.Specifications;
+ using app.web.core;
+ using developwithpassion.specifications.rhinomocks;
+ using developwithpassion.specifications.extensions;
+
+namespace app.specs
+{  
+  [Subject(typeof(ProcessingCommand))]  
+  public class ProcessingCommandSpecs
+  {
+    public abstract class concern : Observes<IProcessOneRequest,
+                                      ProcessingCommand>
+    {
+        
+    }
+
+   
+    public class when_determining_if_it_can_process_a_request : concern
+    {
+
+      Establish c = () =>
+      {
+        request = fake.an<IProvideDetailsForACommand>();
+      };
+
+      Because b = () =>
+        result = sut.can_handle(request);
+
+      It should_ = () =>
+        
+
+      static IProvideDetailsForACommand request;
+      static bool result;
+    }
+  }
+}

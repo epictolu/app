ebca96b34c22192e64431b0134504d7bd3b74937
Jonathan Christian <jchristian>
2011-12-07 13:16:46 -0800

    Pushing new changes

--- source/app/web/core/FrontController.cs
+++ source/app/web/core/FrontController.cs
@@ -2,9 +2,16 @@
 {
   public class FrontController : IProcessRequests
   {
+    IFindCommands command_registry;
+
+    public FrontController(IFindCommands command_registry)
+    {
+      this.command_registry = command_registry;
+    }
+
     public void process(IProvideDetailsForACommand controller_request)
     {
-      throw new System.NotImplementedException();
+      command_registry.get_the_command_that_can_process(controller_request).run(controller_request);
     }
   }
 }

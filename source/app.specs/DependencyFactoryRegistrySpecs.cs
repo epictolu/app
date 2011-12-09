2c8c91152a9aff40dd49d8cc432af5f6b9ff0d9c
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-09 14:12:35 -0700

    Updated

---
+++ source/app.specs/DependencyFactoryRegistrySpecs.cs
@@ -0,0 +1,38 @@
+﻿using Machine.Specifications;
+using app.infrastructure.containers.basic;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+  [Subject(typeof(DependencyFactoryRegistry))]
+  public class DependencyFactoryRegistrySpecs
+  {
+    public abstract class concern : Observes<IFindDependencyFactories,
+                                      DependencyFactoryRegistry>
+    {
+    }
+
+    public class when_finding_a_factory_to_create_a_dependency : concern
+    {
+      public class and_it_has_the_factory
+      {
+        Establish c = () =>
+        {
+          the_factory = fake.an<ICreateASingleDependency>();
+        };
+        Because b = () =>
+          result = sut.get_factory_that_can_create(typeof(AnItem));
+
+        It should_return_the_factory_to_the_caller = () =>
+          result.ShouldEqual(the_factory);
+
+        static ICreateASingleDependency result;
+        static ICreateASingleDependency the_factory;
+      }
+    }
+
+    public class AnItem
+    {
+    }
+  }
+}
---
+++ source/app/infrastructure/containers/basic/DependencyFactoryRegistry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app.infrastructure.containers.basic
+{
+  public class DependencyFactoryRegistry : IFindDependencyFactories
+  {
+    public ICreateASingleDependency get_factory_that_can_create(Type dependency)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
